3923acdcf63a73da7b0f3db6c65a4e6d554aada3
Abdul Haseeb <62133331+askabdulhaseeb@users.noreply.github.com>
2023-04-18 10:35:40 +0500

    import assets

---
+++ lib/utilities/app_images.dart
@@ -0,0 +1,15 @@
+class AppImages {
+  static const String logo = 'assets/images/logo.png';
+  //
+  static const String blockUser = 'assets/images/block-user-button.png';
+  static const String coin = 'assets/images/coin.png';
+  static const String coinsReward = 'assets/images/Coins-reward.png';
+  static const String editDark = 'assets/images/edit-dark.png';
+  static const String editLight = 'assets/images/edit-light.png';
+  static const String giftBox = 'assets/images/gift-box.png';
+  static const String plus = 'assets/images/plus.png';
+  static const String profileLikes = 'assets/images/profile-likes.png';
+  static const String profileQRcode = 'assets/images/profile-qr-code.png';
+  static const String undrawSocial = 'assets/images/undraw_social.png';
+  static const String work = 'assets/images/work.png';
+}
